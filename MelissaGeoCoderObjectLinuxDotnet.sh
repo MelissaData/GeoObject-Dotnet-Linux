9fd795fbca98e42e9e99fecf30d4fab03209591e
Melissa <ha@melissadata.com>
2024-04-24 10:39:04 -0700

    Tested successfully using Melissa data files for 2024.Q2

--- MelissaGeoCoderObjectLinuxDotnet.sh
+++ MelissaGeoCoderObjectLinuxDotnet.sh
@@ -11,56 +11,76 @@ NC='\033[0m' #No Color
 ######################### Parameters ##########################
 
 zip=""
+dataPath=""
 license=""
 quiet="false"
 
 while [ $# -gt 0 ] ; do
-	case $1 in 
-		-z | --zip)
-			zip="$2"
-			
-			if [ "$zip" == "-l" ] || [ "$zip" == "--license" ] || [ "$zip" == "-q" ] || [ "$zip" == "--quiet" ] || [ -z "$zip" ];
-			then 
-				printf "${RED}Error: Missing an argument for parameter \'zip\'.${NC}\n"
-				exit 1
-			fi
-			;;
-		-l | --license)
-			license="$2"
-			
-			if [ "$license" == "-q" ] || [ "$license" == "--quiet" ] || [ "$license" == "-z" ] || [ "$license" == "--zip" ] || [ -z "$license" ];
-			then
-				printf "${RED}Error: Missing an argument for parameter \'license'\.${NC}\n"
-				exit 1
-			fi
-			;;
-		-q | --quiet)
-			quiet="true"
-			;;
-	esac
-	shift
+    case $1 in 
+        --zip)
+            zip="$2"
+            
+            if [ "$zip" == "--dataPath" ] || [ "$zip" == "--license" ] || [ "$zip" == "--quiet" ] || [ -z "$zip" ];
+            then 
+                printf "${RED}Error: Missing an argument for parameter \'zip\'.${NC}\n"
+                exit 1
+            fi
+            ;;
+        --dataPath) 
+            dataPath="$2"
+            
+            if [ "$dataPath" == "--license" ] || [ "$dataPath" == "--quiet" ] || [ "$dataPath" == "--zip" ] || [ -z "$dataPath" ];
+            then
+                printf "${RED}Error: Missing an argument for parameter \'dataPath\'.${NC}\n"  
+                exit 1
+            fi  
+            ;;
+        --license)
+            license="$2"
+            
+            if [ "$license" == "--quiet" ] || [ "$license" == "--zip" ] || [ "$license" == "--dataPath" ] || [ -z "$license" ];
+            then
+                printf "${RED}Error: Missing an argument for parameter \'license'\.${NC}\n"
+                exit 1
+            fi
+            ;;
+        --quiet)
+            quiet="true"
+            ;;
+    esac
+    shift
 done
 
 ######################### Config ###########################
 
-RELEASE_VERSION='2024.01'
+RELEASE_VERSION='2024.Q2'
 ProductName="GEOCODER_DATA"
 
 # Uses the location of the .sh file 
-# Modify this if you want to use 
 CurrentPath=$(pwd)
 ProjectPath="$CurrentPath/MelissaGeoCoderObjectLinuxDotnet"
-BuildPath="$ProjectPath/Build"
-DataPath="$ProjectPath/Data"
 
-if [ ! -d $DataPath ];
+BuildPath="$ProjectPath/Build"
+if [ ! -d "$BuildPath" ];
 then
-    mkdir $DataPath
+    mkdir "$BuildPath"
 fi
 
-if [ ! -d $BuildPath ];
+if [ -z "$dataPath" ];
 then
-    mkdir $BuildPath
+    DataPath="$ProjectPath/Data"
+else
+    DataPath=$dataPath
+fi
+
+if [ ! -d "$DataPath" ] && [ "$DataPath" == "$ProjectPath/Data" ];
+then
+    mkdir "$DataPath"
+elif [ ! -d "$DataPath" ] && [ "$DataPath" != "$ProjectPath/Data" ];
+then
+    printf "\nData file path does not exist. Please check that your file path is correct.\n"
+    printf "\nAborting program, see above.\n"
+    exit 1
 fi
 
 # Config variables for download file(s)
@@ -179,18 +199,25 @@ then
   exit 1
 fi
 
+# Get data file path (either from parameters or user input)
+if [ "$DataPath" = "$ProjectPath/Data" ]; then
+    printf "Please enter your data files path directory if you have already downloaded the release zip.\nOtherwise, the data files will be downloaded using the Melissa Updater (Enter to skip): "
+    read dataPathInput
+
+    if [ ! -z "$dataPathInput" ]; then  
+        if [ ! -d "$dataPathInput" ]; then  
+            printf "\nData file path does not exist. Please check that your file path is correct.\n"
+            printf "\nAborting program, see above.\n"
+            exit 1
+        else
+            DataPath=$dataPathInput
+        fi
+    fi
+fi
+
 # Use Melissa Updater to download data file(s) 
 # Download data file(s) 
-DownloadDataFiles $license      # comment out this line if using DQS Release
-
-# Set data file(s) path
-#DataPath=""      # uncomment this line and change to your DQS Release data file(s) directory 
-
-#if [ ! -d $DataPath ]; # uncomment this section of code if you are using your own DQS Release data file(s) directory
-#then
-    #printf "\nData path is invalid!\n"
-    #exit 1
-#fi
+DownloadDataFiles $license # Comment out this line if using own release
 
 # Download SO(s)
 DownloadSO $license 
